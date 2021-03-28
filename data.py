8fe7bc0d8eb305a718a9f3aa71c4d4dcedca9150
David Zhao <david.zhao@hotmail.co.uk>
2021-03-28 22:39:36 +0100

    Basic flask skeleton without database

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'lorem fihdiegfjerg',
+            'author': 'DZ',
+            'create_date': '04-24-2019'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'lorem fihdiegfjerg',
+            'author': 'DZ',
+            'create_date': '04-24-2019'
+        }
+    ]
+    return articles
